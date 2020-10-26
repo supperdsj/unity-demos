a48f6bbf009b830cd0fc4ef1da5f87d9d471b1a7
董劭杰 <232171773@qq.com>
2020-10-26 17:29:07 +0800

    完成移动和部分碰撞

---
+++ course-25-mini-games/TankGame/Assets/Scripts/Tank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Tank : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float xInput;
+    private SpriteRenderer sprite;
+    public float speed = 5f;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        xInput = Input.GetAxis("Horizontal");
+        if (xInput < 0)
+        {
+            sprite.flipX = true;
+        }else if (xInput > 0)
+        {
+            sprite.flipX = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = Vector2.right * (xInput * speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // Destroy(collision.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
