5226a06b0a9bb1a9fcc946e03fa0e736fd50de41
董劭杰 <232171773@qq.com>
2020-10-27 13:56:40 +0800

    finish inf runner

---
+++ course-25-mini-games/InfiniteScrolling/Assets/Script/Background.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    private Material material;
+    private Vector2 offset;
+    public int xVelocity, yVelocity;
+
+    private void Awake()
+    {
+        material = GetComponent<Renderer>().material;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = new Vector2(xVelocity, yVelocity);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        material.mainTextureOffset += offset * Time.deltaTime;
+    }
+}
