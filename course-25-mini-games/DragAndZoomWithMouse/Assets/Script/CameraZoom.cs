642796eb344c7246079a7d5b2a8271651eefc27d
董劭杰 <232171773@qq.com>
2020-10-27 14:36:00 +0800

    rename project and finish zoom

---
+++ course-25-mini-games/DragAndZoomWithMouse/Assets/Script/CameraZoom.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CameraZoom : MonoBehaviour
+{
+    public int zoomSpeed2D;
+    public int zoomSpeed3D;
+    private int minZoom2D = 1;
+    private int maxZoom2D = 5;
+    private int minZoom3D = 10;
+    private int maxZoom3D = 100;
+    void Update()
+    {
+        if (Camera.main.orthographic)
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                Camera.main.orthographicSize -= zoomSpeed2D * Time.deltaTime;
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                Camera.main.orthographicSize += zoomSpeed2D * Time.deltaTime;
+            }
+
+            Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize, minZoom2D, maxZoom2D);
+        }
+        else
+        {
+            if (Input.GetAxis("Mouse ScrollWheel") > 0)
+            {
+                Camera.main.fieldOfView -= zoomSpeed3D * Time.deltaTime;
+            }
+            else if (Input.GetAxis("Mouse ScrollWheel") < 0)
+            {
+                Camera.main.fieldOfView += zoomSpeed3D * Time.deltaTime;
+            }
+            Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, minZoom3D, maxZoom3D);
+        }
+    }
+}
