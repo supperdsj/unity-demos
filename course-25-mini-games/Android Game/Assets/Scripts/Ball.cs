72fbe9f3b1eebcbd2b4bf464dac618c08ecb79b7
董劭杰 <232171773@qq.com>
2020-10-28 11:38:08 +0800

    finish android game

---
+++ course-25-mini-games/Android Game/Assets/Scripts/Ball.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        GameObject.FindObjectOfType<GameManager>().GetComponent<GameManager>().ScoreUp();
+        Destroy(gameObject);
+    }
+}
---
+++ course-25-mini-games/Android Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    private int score = 0;
+    public GameObject winText;
+    public GameObject restartBtn;
+
+    private void Awake()
+    {
+        if (Application.platform == RuntimePlatform.IPhonePlayer)
+        {
+            Application.targetFrameRate = 60;
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ScoreUp()
+    {
+        score++;
+        // if (!GameObject.FindObjectOfType<Ball>())
+        if(score>=4)
+        {
+            Win();
+        }
+    }
+
+    void Win()
+    {
+        winText.SetActive(true);
+        restartBtn.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
